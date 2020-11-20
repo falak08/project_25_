538b317e7791ccdecdd653f8f4fe5448ac48cf09
falak08 <73591794+falak08@users.noreply.github.com>
2020-11-20 19:33:00 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,52 @@
+class dustbin
+{
+	constructor(x,y)
+	{
+		this.x=x;
+		this.y=y;
+		this.dustbinWidth=40;
+		this.dustbinHeight=20;
+		this.wallThickness=20;
+		this.angle=0;	
+		
+		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
+		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		Matter.Body.setAngle(this.leftWallBody, this.angle);
+		
+		this.image=loadImage("sprites/dustbingreen.png");
+
+		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		Matter.Body.setAngle(this.rightWallBody, -1*this.angle);
+		World.add(world, this.bottomBody)
+		World.add(world, this.leftWallBody)
+		World.add(world, this.rightWallBody);
+
+	}
+	display()
+	{
+			var posBottom=this.bottomBody.position;
+			var posLeft=this.leftWallBody.position;
+			var posRight=this.rightWallBody.position;
+
+			
+
+			push()
+			translate(posLeft.x, posLeft.y);
+			
+			pop()
+
+			push()
+			translate(posRight.x, posRight.y);
+			imageMode(CENTER);
+        image(this.image, 0, 0, this.x, this.y);
+			pop()
+
+			push()
+			translate(posBottom.x, posBottom.y);
+		pop()
+
+			
+			
+	}
+
+}
